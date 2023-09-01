0e14d9478e2b4a99dfb94785f396cc1903d0a3ea
Immugio <janmikeska@gmail.com>
2023-09-01 12:31:01 +0200

    Update TypeScript version to 5.2.2, add bind method, and adjust tsconfig.json settings.

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { clearChildren } from "./clearChildren";
 export { getElementAsync } from "./getElementAsync";
 export { isElementChildOf } from "./isElementChildOf";
 export { dom, AnyContent, BaseProps, JsxProps, IRef } from "./JSX";
+export { bind } from "./bind";
